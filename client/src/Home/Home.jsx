067f6dc3d734192f8a518ede5a42bca4f1092834
DarisMavric <mvrcdaris@gmail.com>
2024-12-27 01:19:21 +0100

    Created Template For Home Page

--- client/src/Home/Home.jsx
+++ client/src/Home/Home.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
-import "./Home.css"
-import Navbar from '../components/Navbar/Navbar'
+import React from "react";
+import "./Home.css";
+import Navbar from "../components/Navbar/Navbar";
+import icon from "./user-avatar-male-5.png";
+import { FaUserFriends } from "react-icons/fa";
+import { IoChatbox } from "react-icons/io5";
 
 const Home = () => {
   return (
     <div className="home">
-        <Navbar/>
+      <Navbar />
+      <div className="chatter">
+        <div className="chat-and-contacts">
+          <div className="contacts">
+            <div className="all-contacts">
+              <FaUserFriends style={{ width: 35, height: 35 }} />
+              <h1>Contacts</h1>
+            </div>
+            <div className="contact">
+              <div className="contact-image">
+                <img src={icon} alt="" />
+              </div>
+              <div className="contact-name">
+                <h2>Daris Mavric</h2>
+                <p>Online</p>
+              </div>
+            </div>
+            <div className="contact">
+              <div className="contact-image">
+                <img src={icon} alt="" />
+              </div>
+              <div className="contact-name">
+                <h2>Daris Mavric</h2>
+                <p>Online</p>
+              </div>
+            </div>
+          </div>
+          <div className="chat">
+            <IoChatbox style={{width: 100,height: 100}}/>
+            <h1>Welcome To Chatter</h1>
+            <p>Select a conversation from the sidebar to start chatting</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,18 +4,20 @@ import "./Navbar.css"
 import { FaUserFriends } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
 import { RiLogoutBoxFill } from "react-icons/ri";
+import { IoChatbox } from "react-icons/io5";
 
 const Navbar = () => {
   return (
     <div className="navbar">
         <div className="logo">
-            <img src={icon} alt="Logo" />
+            <IoChatbox style={{width: 45,height: 45}}/>
+            <h1>Chatter</h1>
         </div>
         <div className="links">
           <ul>
-            <li><FaUserFriends style={{width: 35,height: 35}}/><p>Add Friend</p></li>
-            <li><CgProfile style={{width: 35,height: 35}}/><p>Profile</p></li>
-            <li><RiLogoutBoxFill style={{width: 35,height: 35}}/><p>Logout</p></li>
+            <li><FaUserFriends style={{width: 25,height: 25}}/><p>Add Friend</p></li>
+            <li><CgProfile style={{width: 25,height: 25}}/><p>Profile</p></li>
+            <li><RiLogoutBoxFill style={{width: 25,height: 25}}/><p>Logout</p></li>
           </ul>
         </div>
     </div>
