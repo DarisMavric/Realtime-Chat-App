659294c69f22728e895fd872675004d1a9a96dc5
DarisMavric <mvrcdaris@gmail.com>
2024-12-28 22:51:11 +0100

    Created Sign Up Page

--- client/src/Login/Login.jsx
+++ client/src/Login/Login.jsx
@@ -1,33 +1,44 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
 import { IoChatbox } from "react-icons/io5";
-import "./Login.css"
+import "./Login.css";
+import { MdEmail } from "react-icons/md";
+import { FaLock } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div className="login-page">
-      <Navbar/>
+      <Navbar />
       <div className="login-section">
         <div className="login-form">
           <div className="login-form-header">
-            <IoChatbox style={{width: 100,height: 100}}/>
+            <IoChatbox style={{ width: 100, height: 100 }} />
             <h1>Welcome Back</h1>
             <p>Sign in to your account</p>
           </div>
           <div className="login-form-body">
             <label for="email">Email</label>
-            <input type="text" name='email'/>
+            <div className="input-container">
+              <input type="text" name="email" />
+              <MdEmail className="icon" />
+            </div>
             <label for="password">Password</label>
-            <input type="password" name='password'/>
+            <div className="input-container">
+              <input type="password" name="password" />
+              <FaLock className="icon" />
+            </div>
             <button>Sign in</button>
           </div>
           <div className="login-form-footer">
-            <p>Don't have an account?<a href="">Create account</a></p>
+            <p>
+              Don't have an account?<Link to="/signup" className="link">Sign up</Link>
+            </p>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- client/src/Register/Register.jsx
+++ client/src/Register/Register.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import { IoPerson } from "react-icons/io5";
+import { IoChatbox } from "react-icons/io5";
+import { MdEmail } from "react-icons/md";
+import { FaLock } from "react-icons/fa";
+import "./Register.css"
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="register-page">
+      <Navbar />
+      <div className="register-section">
+        <div className="register-form">
+          <div className="register-form-header">
+            <IoChatbox style={{ width: 100, height: 100 }} />
+            <h1>Create Account</h1>
+            <p>Get started with your free account</p>
+          </div>
+          <div className="register-form-body">
+            <label for="fullName">Full Name</label>
+            <div className="input-container">
+                <input type="text" name="fullName"/>
+                <IoPerson className="icon"/>
+            </div>
+            <label for="email">Email</label>
+            <div className="input-container">
+                <input type="text" name="email" />
+                <MdEmail className="icon"/>
+            </div>
+            <label for="password">Password</label>
+            <div className="input-container">
+                <input type="password" name="password" />
+                <FaLock className="icon"/>
+            </div>
+            <button>Sign Up</button>
+          </div>
+          <div className="register-form-footer">
+            <p>
+              Already have an account?<Link to="/login" className="link">Sign in</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
