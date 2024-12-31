3617efb060e9538d5e7f8811b8abf95b10ae3813
DarisMavric <mvrcdaris@gmail.com>
2025-01-01 00:58:32 +0100

    Navbar Redesign

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,27 +1,50 @@
-import React from 'react'
-import icon from './Chatter.png'
-import "./Navbar.css"
+import React from "react";
+import icon from "./Chatter.png";
+import "./Navbar.css";
 import { FaUserFriends } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
 import { RiLogoutBoxFill } from "react-icons/ri";
-import { IoChatbox } from "react-icons/io5";
+import { FaSnapchat } from "react-icons/fa6";
+import { BsChatRightDots } from "react-icons/bs";
 
 const Navbar = () => {
   return (
     <div className="navbar">
+      <div className="logo-and-links">
         <div className="logo">
-            <IoChatbox style={{width: 45,height: 45}}/>
-            <h1>Chatter</h1>
+          <FaSnapchat/>
         </div>
         <div className="links">
           <ul>
-            <li><FaUserFriends style={{width: 25,height: 25}}/><p>Add Friend</p></li>
-            <li><CgProfile style={{width: 25,height: 25}}/><p>Profile</p></li>
-            <li><RiLogoutBoxFill style={{width: 25,height: 25}}/><p>Logout</p></li>
+            <li>
+              <FaUserFriends />
+            </li>
+            <li>
+              <CgProfile />
+            </li>
+            <li>
+              <RiLogoutBoxFill />
+            </li>
+            <li>
+              <BsChatRightDots />
+            </li>
           </ul>
         </div>
+      </div>
+      <div className="navbar-footer">
+      <div className="links">
+          <ul>
+            <li>
+              <FaUserFriends />
+            </li>
+            <li>
+              <CgProfile />
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
